bd4be5adc2ecc94a14af0fa0c4e77c42530e908b
adibest <ahmadjayadibya@gmail.com>
2019-04-08 11:48:32 +0700

    bareng pakdhe yahya, ngegit gone add, gurong rampung

---
+++ model-y/app/Model/Profil.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profil extends Model
+{
+    protected $table = 'profil';
+
+    protected $fillable = ['depan', 'belakang', 'alamat', 'avatar'];
+
+    public function getNamaAttribute()
+    {
+    	$out = "{$this->depan} {$this->belakang}";
+    	$me = strtoupper($out);
+
+    	return $me;
+    }
+}
---
+++ model-y/resources/views/profil.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil Accessor</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container">
+		<div class="jumbotron">
+			<a href="/profil">
+				<h1>Profil</h1>
+			</a>
+		</div>
+		<form method="post" action="{{ route('profil.store') }}">
+			<div class="form-row">
+				<div class="row-auto">
+					<input type="text" name="depan" class="form-control" placeholder="Nama Depan">
+				</div>
+				<div class="row-auto">
+					<input type="text" name="belakang" class="form-control" placeholder="Nama Belakang">
+				</div>
+				<div class="row-auto">
+					<input type="text" name="alamat" class="form-control" placeholder="Alamat">
+				</div>
+				<div class="row-auto">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</div>
+		</form>
+		<a href="{{ route('profil.index') }}" class="btn btn-sm btn-primary">List</a>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
